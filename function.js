439df2ecc9bb64d75722b77551aadbf0b0c8b217
Tino <tinomhedziso21@gmail.com>
2024-07-11 12:57:22 +0200

    Update

--- function.js
+++ function.js
@@ -50,14 +50,12 @@ function display()
             title.textContent = "English";
             desc.textContent ="Elevate your English skills with The Learning Curve's premier tutoring services. Our experienced tutors specialize in enhancing comprehension, grammar, writing, and critical analysis skills."+
                      "Through personalized sessions, we foster a deep understanding of literature and language, empowering students to articulate their thoughts effectively."+
-                     "Whether preparing for exams, honing writing abilities, or exploring literary classics, The Learning Curve provides the tools and support needed for academic excellence in English."
                      "Step into success with us and watch your proficiency soar."; break;
         }
         case 3 : 
         {
             title.textContent = "Math";
             desc.textContent = "Unlock your potential in mathematics with The Learning Curve's expert tutoring services." +
-                     "Our dedicated tutors provide tailored guidance to help students grasp concepts with confidence, ensuring each individual achieves academic success." +
                      "Are you struggling with tackling algebra, calculus, or geometry? Fear Not: our proven methods and personalized approach empower students to excel in their mathematical studies." +
                      "Join us at The Learning Curve and experience the difference in your mathematical journey."; break;
         }
