beb36bd8ca80bc656d4123425dff3fb291f4d7ba
Tino <tinomhedziso21@gmail.com>
2024-07-12 10:21:53 +0200

    Update
    Co-Authored-By: Tino <129593058+passionoverpain@users.noreply.github.com>
    Co-Authored-By: Generic account User <173554401+onlyjustjayden@users.noreply.github.com>

--- function.js
+++ function.js
@@ -177,8 +177,8 @@ function book(Name,Surname,Grade,Email)
     {
     const encodedSubject = encodeURIComponent('Requesting Tutoring Class');
     date = datePicker.value;
-    const encodedBody = encodeURIComponent(`Good Day The Learning Curve Team, my name is ${Name} ${Surname}, a learner in grade ${Grade}. I would like to book tutoring classes for ${subjects} that will start ${date}. i have  also selected a ${tutPlan} for my learning period. Please feel free to respond to this message at ${Email}`);
-    window.alert(`Thnak you for reaching out to us ${Name}. We will  be in touch with you at ${Email} shortly`);
+    const encodedBody = encodeURIComponent(`Good Day The Learning Curve Team, my name is ${Name} ${Surname}, a learner in grade ${Grade}. I would like to book tutoring classes for ${subjects} that will start ${date}. I have  also selected a ${tutPlan} plan for my learning period. Please feel free to respond to this message at ${Email}`);
+    window.alert(`Thank you for reaching out to us ${Name}. We will  be in touch with you at ${Email} shortly`);
     const link = `mailto:tinomhedziso@gmail.com?subject=${encodedSubject}&body=${encodedBody}`;
     window.open(link,`_blank`);
     countsub = 0;
